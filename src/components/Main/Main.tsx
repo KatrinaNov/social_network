737c13dd25670ed3a0580f25388313a9e41c63a4
katrina <katrina.nov13@gmail.com>
2021-11-12 19:32:53 +0300

    add router

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -2,15 +2,21 @@ import React from 'react';
 import './Main.css';
 import Navbar from "../Navbar/Navbar";
 import Dialogs from "../Dialogs/Dialogs";
-// import Profile from "../Profile/Profile";
+import Profile from "../Profile/Profile";
+import {Route, BrowserRouter, Routes} from 'react-router-dom';
 
 const Main = () => {
   return (
+    // eslint-disable-next-line react/jsx-no-undef
+    <BrowserRouter>
     <main className='main'>
       <Navbar/>
-      {/*<Profile/>*/}
-      <Dialogs/>
+      <Routes>
+        <Route path='/profile' element={<Profile/>}/>
+        <Route path='/dialogs' element={<Dialogs/>}/>
+      </Routes>
     </main>
+    </BrowserRouter>
   );
 }
 export default Main;
