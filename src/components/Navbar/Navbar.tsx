581e5d8e27f4c68c7a2772d94961b4bdf5bd3b2c
katrina <katrina.nov13@gmail.com>
2021-11-12 21:52:19 +0300

    refact a to NavLink

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,15 +1,17 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 import s from './Navbar.module.css';
 
 const Navbar = () => {
   return (
     <nav className={s.nav}>
       <ul>
-        <li className={`${s.item} ${s.active}`}><a href="/profile">Profile</a></li>
-        <li className={s.item}><a href="/dialogs">Messages</a></li>
-        <li className={s.item}><a href="/news">News</a></li>
-        <li className={s.item}><a href="/music">Music</a></li>
-        <li className={s.item}><a href="/settings">Settings</a></li>
+        {/* eslint-disable-next-line react/jsx-no-undef */}
+        <li className={`${s.item} ${s.active}`}><NavLink to="/profile" className={(navActive) => navActive.isActive ? s.active : ""}>Profile</NavLink></li>
+        <li className={s.item}><NavLink to="/dialogs" className={(navActive) => navActive.isActive ? s.active : ""}>Messages</NavLink></li>
+        <li className={s.item}><NavLink to="/news" className={(navActive) => navActive.isActive ? s.active : ""}>News</NavLink></li>
+        <li className={s.item}><NavLink to="/music" className={(navActive) => navActive.isActive ? s.active : ""}>Music</NavLink></li>
+        <li className={s.item}><NavLink to="/settings" className={(navActive) => navActive.isActive ? s.active : ""}>Settings</NavLink></li>
       </ul>
     </nav>
   );
