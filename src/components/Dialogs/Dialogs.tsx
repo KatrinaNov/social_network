663a43db4bb0fc849a47cb89633bd0eee70c792d
katrina <katrina.nov13@gmail.com>
2021-11-17 19:51:18 +0300

    26. Components DialogItem and Message

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -1,27 +1,8 @@
 import React from 'react';
 import s from './Dialogs.module.css';
-import {NavLink} from "react-router-dom";
+import DialogItem from "./DialogItem/DialogItem";
+import Message from "./Message/Message";
 
-type DialogItemType = {
-  name: string
-  id: number
-}
-type MessageType = {
-  message: string
-  id: number
-}
-
-const DialogItem = (props: DialogItemType) => {
-  let path = `/dialogs/${props.id}`
-  return (
-    <div className={s.dialog}>
-      <NavLink to={path}>{props.name}</NavLink>
-    </div>
-  )
-}
-const Message = (props: MessageType) => {
-  return <div className={s.message}>{props.message}</div>
-}
 const Dialogs = () => {
   let dialogs = [
     {id: 1, name: 'Dimych'},
