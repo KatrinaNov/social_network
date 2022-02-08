a69a74f374d960471c39d59f0c3e09e10b8e6210
katrina <katrina.nov13@gmail.com>
2022-02-08 20:51:57 +0300

    72. refact bind, add arrow function

--- src/components/Profile/ProfileInfo/ProfileStatus.tsx
+++ src/components/Profile/ProfileInfo/ProfileStatus.tsx
@@ -9,12 +9,12 @@ class ProfileStatus extends React.Component <ProfileStatusPropsType> {
   state = {
     editMode: false
   }
-  activateEditMode() {
+  activateEditMode = () => {
     this.setState({
       editMode: true
     })
   }
- deactivateEditMode() {
+ deactivateEditMode = () => {
     this.setState({
       editMode: false
     })
@@ -24,13 +24,13 @@ render() {
   return (
     <>{
       !this.state.editMode && <div>
-          <span onDoubleClick={this.activateEditMode.bind(this)}>{this.props.status}</span>
+          <span onDoubleClick={this.activateEditMode}>{this.props.status}</span>
       </div>
     }
       {this.state.editMode && <div>
           <input
               value={this.props.status}
-              onBlur={this.deactivateEditMode.bind(this)}
+              onBlur={this.deactivateEditMode}
               autoFocus={true}
               // onChange={}
           />
